7944cd3938159d20cebcd72c8e556900e49dab68
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-22 22:10:53 +0530

    added herotext for mobile view

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,3 +1,6 @@
+import FlipWords from "./FlipWords";
+
+const words = ["Secure", "Modern", "Scalable"];
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
@@ -8,8 +11,28 @@ const HeroText = () => {
             A Developer <br />
             dedicated to Crafting
           </p>
-          {/* <div className=""></div> */}
+          <div>
+            <FlipWords
+              words={words}
+              className="font-black text-white text-8xl"
+            />
+          </div>
           <p className="text-4xl font-medium text-neutral-300">Web Solutions</p>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-6 md:hidden">
+        <p className="text-4xl font-medium">Hi I'm Vikrant</p>
+        <div>
+          <p className="text-5xl font-black text-neutral-300">Building</p>
+          <div>
+            <FlipWords
+              words={words}
+              className="font-bold text-white text-7xl"
+            />
+          </div>
+          <p className="text-4xl font-black text-neutral-300">
+            Web Applications
+          </p>
         </div>
       </div>
     </div>
