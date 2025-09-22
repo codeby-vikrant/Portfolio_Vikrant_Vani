6c82c40215fdac8390acb6b709a366244465939e
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-22 23:27:59 +0530

    added parallax effect and scroll motion to images

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,4 +1,13 @@
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
+
 const ParallaxBackground = () => {
+  const { scrollYProgress } = useScroll();
+  const x = useSpring(scrollYProgress, { damping: 50 });
+  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"]);
+  const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
+  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
+  const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
+
   return (
     <section className="absolute inset-0 bg-black/40">
       <div className="relative h-screen overflow-y-hidden">
@@ -10,36 +19,40 @@ const ParallaxBackground = () => {
             backgroundSize: "cover",
           }}
         />
-        <div
+        <motion.div
           className="absolute inset-0 -z-40"
           style={{
             backgroundImage: "url(/assets/mountain-3.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            y: mountain3Y,
           }}
         />
-        <div
+        <motion.div
           className="absolute inset-0 -z-30"
           style={{
             backgroundImage: "url(/assets/planets.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            x: planetsX,
           }}
         />
-        <div
+        <motion.div
           className="absolute inset-0 -z-20"
           style={{
             backgroundImage: "url(/assets/mountain-2.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            y: mountain2Y,
           }}
         />
-        <div
+        <motion.div
           className="absolute inset-0 -z-10"
           style={{
             backgroundImage: "url(/assets/mountain-1.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            y: mountain1Y,
           }}
         />
       </div>
