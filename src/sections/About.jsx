19c9bd80ddb5eb68338e872d17cd17f19cfe7f9a
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-23 21:40:49 +0530

    added card sections for about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,16 @@
 const About = () => {
-  return <div></div>;
+  return (
+    <section className="c-space section-spacing">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        <div className="flex items-end grid-default-color grid-1"></div>
+        <div className="grid-default-color grid-2"></div>
+        <div className="grid-black-color grid-3"></div>
+        <div className="grid-special-color grid-4"></div>
+        <div className="grid-default-color grid-5"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
