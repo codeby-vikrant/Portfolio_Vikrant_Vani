027641e67f49beef0ff10e47bbe8201997fb3477
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-23 21:56:17 +0530

    updated design of card using image and text

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,7 +3,21 @@ const About = () => {
     <section className="c-space section-spacing">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-        <div className="flex items-end grid-default-color grid-1"></div>
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            alt="Coding Image"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Vikrant Vani</p>
+            <p className="subtext">
+              Over the last 4 years, I developed my frontend and backend
+              development skills to deliver dynamic and responsive software and
+              mobile/web applications
+            </p>
+          </div>
+        </div>
         <div className="grid-default-color grid-2"></div>
         <div className="grid-black-color grid-3"></div>
         <div className="grid-special-color grid-4"></div>
