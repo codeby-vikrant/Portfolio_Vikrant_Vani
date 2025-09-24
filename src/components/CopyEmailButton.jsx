16cf198fe74146cf299b39cec39e4cc1b7e423ce
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 12:00:28 +0530

    updated copy state to show text after copied and reset it

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -16,10 +16,17 @@ const CopyEmailButton = () => {
       className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden"
       onClick={copyToClipboard}
     >
-      <p className="flex items-center justify-center gap-2">
-        <img src="assets/copy.svg" alt="copy icon" className="w-5" />
-        Copy Email Address
-      </p>
+      {copied ? (
+        <p className="flex items-center justify-center gap-2">
+          <img src="assets/copy-done.svg" alt="copy done" className="w-5" />
+          Email has copied
+        </p>
+      ) : (
+        <p className="flex items-center justify-center gap-2">
+          <img src="assets/copy.svg" alt="copy icon" className="w-5" />
+          Copy Email Address
+        </p>
+      )}
     </button>
   );
 };
