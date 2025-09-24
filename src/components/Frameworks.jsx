be7bfd1e90fa4e599615438f0f0b396be94859d1
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 14:46:29 +0530

    removed unnecessary css and reversed the inner circle

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -16,14 +16,14 @@ export function Frameworks() {
     "tailwind",
   ];
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
           <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
-      <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
-        {skills.map((skill, index) => (
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
           <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
