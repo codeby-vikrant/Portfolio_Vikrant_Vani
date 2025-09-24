a5d24f8b56fe14974b8d0c706bac026ce5e350b2
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 21:25:44 +0530

    called details in project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,15 @@
-const Project = () => {
+import ProjectDetails from "./ProjectDetails";
+
+const Project = ({ title, description, subDescription, href, image, tags }) => {
   return (
     <>
       <div className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0">
         <div>
-          <p className="text-2xl">Title</p>
+          <p className="text-2xl">{title}</p>
           <div className="flex gap-5 mt-2 text-sand">
-            <span>Tag1</span>
-            <span>Tag2</span>
-            <span>Tag3</span>
+            {tags.map((tag) => (
+              <span key={tag.id}>{tag.name}</span>
+            ))}
           </div>
         </div>
         <button className="flex items-center gap-1 cursor-pointer hover-animation">
@@ -16,6 +18,7 @@ const Project = () => {
         </button>
       </div>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      <ProjectDetails />
     </>
   );
 };
