8460a0ff4f5a74624e3057265f2aa375306dfaf4
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 22:17:02 +0530

    added required metadata

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import ProjectDetails from "./ProjectDetails";
 
-const Project = ({ title, description, subDescription, href, image, tags }) => {
+const Project = ({
+  title,
+  description,
+  subDescription,
+  href,
+  image,
+  tags,
+  setPreview,
+}) => {
+  const [isHidden, setIsHidden] = useState(false);
   return (
     <>
-      <div className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0">
+      <div
+        className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0"
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+      >
         <div>
           <p className="text-2xl">{title}</p>
           <div className="flex gap-5 mt-2 text-sand">
@@ -12,13 +26,26 @@ const Project = ({ title, description, subDescription, href, image, tags }) => {
             ))}
           </div>
         </div>
-        <button className="flex items-center gap-1 cursor-pointer hover-animation">
+        <button
+          className="flex items-center gap-1 cursor-pointer hover-animation"
+          onClick={() => setIsHidden(true)}
+        >
           Read More
           <img src="assets/arrow-right.svg" alt="arrow right" className="w-5" />
         </button>
       </div>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
-      <ProjectDetails />
+      {isHidden && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          image={image}
+          tags={tags}
+          href={href}
+          closeModal={() => setIsHidden(false)}
+        />
+      )}
     </>
   );
 };
