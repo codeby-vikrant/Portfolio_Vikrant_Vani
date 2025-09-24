d2d921c6898723cf5048f7122b8596b4e5837df7
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 11:56:36 +0530

    created a state where users can copy the email address

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 const CopyEmailButton = () => {
+  const [copied, setCopied] = useState(false);
+  const email = "workwithvikrant0111@gmail.com";
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(email);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
   return (
-    <button className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden">
+    <button
+      className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden"
+      onClick={copyToClipboard}
+    >
       <p className="flex items-center justify-center gap-2">
         <img src="assets/copy.svg" alt="copy icon" className="w-5" />
         Copy Email Address
