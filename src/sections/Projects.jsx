a8532df1cd66b81e26ca1fd805f0c57612779f23
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 21:25:29 +0530

    added spread operator

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       <h2 className="text-heading">My Selected Projects</h2>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
       {myProjects.map((project) => (
-        <Project key={project.id}/>
+        <Project key={project.id} {...project}/>
       ))}
     </section>
   );
