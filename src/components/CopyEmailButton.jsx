a4b015b55509ace1e7d6877216872956dc13cd55
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 11:53:19 +0530

    created a button with copy text and icon

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -1,5 +1,12 @@
 const CopyEmailButton = () => {
-  return <div></div>;
+  return (
+    <button className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden">
+      <p className="flex items-center justify-center gap-2">
+        <img src="assets/copy.svg" alt="copy icon" className="w-5" />
+        Copy Email Address
+      </p>
+    </button>
+  );
 };
 
 export default CopyEmailButton;
