782707e197d3f78a9e0d1618677d8a61a078ad45
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 20:33:10 +0530

    created a map to display projects with their metadata

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,15 @@
+import Project from "../components/Project";
+import { myProjects } from "../constants";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="relative c-space section-spacing">
+      <h2 className="text-heading">My Selected Projects</h2>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
+      {myProjects.map((project) => (
+        <Project key={project.id}/>
+      ))}
+    </section>
+  );
 };
 
 export default Projects;
