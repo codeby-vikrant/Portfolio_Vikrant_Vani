d3034e1ef2dddaa18c0e2b2d22f79caa67cf2e08
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 20:32:49 +0530

    created project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <div className="flex-wrap items-center justify-baseline space-y-14 sm:flex sm:space-y-0">
+      Project
+    </div>
+  );
+};
+
+export default Project;
