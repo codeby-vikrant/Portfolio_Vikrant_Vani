c36aeb55c6827bc2d8f5c743edb10a2347c2cba1
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 14:30:41 +0530

    created a rotating semi circle of skills

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,5 +1,36 @@
-const Frameworks = () => {
-  return <div></div>;
-};
+import { OrbitingCircles } from "./OrbitingCircles";
 
-export default Frameworks;
+export function Frameworks() {
+  const skills = [
+    "android",
+    "swift",
+    "flutter",
+    "react",
+    "javascript",
+    "java",
+    "nodejs",
+    "git",
+    "mysql",
+    "mongodb",
+    "firebase",
+    "tailwind",
+  ];
+  return (
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+const Icon = ({ src }) => (
+  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);
