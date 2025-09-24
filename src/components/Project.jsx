26d71206fdf7974aee500328dd4fc09a741ca211
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-24 21:02:04 +0530

    added read more button along with icon and project sections

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,22 @@
 const Project = () => {
   return (
-    <div className="flex-wrap items-center justify-baseline space-y-14 sm:flex sm:space-y-0">
-      Project
-    </div>
+    <>
+      <div className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0">
+        <div>
+          <p className="text-2xl">Title</p>
+          <div className="flex gap-5 mt-2 text-sand">
+            <span>Tag1</span>
+            <span>Tag2</span>
+            <span>Tag3</span>
+          </div>
+        </div>
+        <button className="flex items-center gap-1 cursor-pointer hover-animation">
+          Read More
+          <img src="assets/arrow-right.svg" alt="arrow right" className="w-5" />
+        </button>
+      </div>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+    </>
   );
 };
 
