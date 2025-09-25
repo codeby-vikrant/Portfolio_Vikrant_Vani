d9f825d5a536d26fa30c4c770a9422a8e2def747
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-09-25 21:02:36 +0530

    updated footer design and imported it

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Experiences />
       <Projects />
       <Contact />
+      <Footer/>
     </div>
   );
 };
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,25 @@
+import { mySocials } from "../constants";
+
 const Footer = () => {
-  <div></div>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+      <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      <div className="flex gap-2">
+        <p>Terms and Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div className="flex gap-3">
+        {mySocials.map((social, index) => (
+          <a href={social.href} key={index}>
+            <img src={social.icon} alt={social.name} className="w-5 h-5" />
+          </a>
+        ))}
+      </div>
+      <p>&copy; {currentYear} Vikrant Vani. All rights reserved.</p>
+    </section>
+  );
 };
 
 export default Footer;
