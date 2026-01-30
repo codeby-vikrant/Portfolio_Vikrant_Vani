9ef9d5312f43c90f0de43f9cde01e735e8271e0c
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-30 22:53:03 +0530

    will update later

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -5,16 +5,16 @@ function Navigation() {
   return (
     <ul className="nav-ul">
       <li className="nav-li">
-        <a className="nav-link">Home</a>
+        {/* <a className="nav-link">Home</a> */}
       </li>
       <li className="nav-li">
-        <a className="nav-link">About</a>
+        {/* <a className="nav-link">About</a> */}
       </li>
       <li className="nav-li">
-        <a className="nav-link">Work</a>
+        {/* <a className="nav-link">Work</a> */}
       </li>
       <li className="nav-li">
-        <a className="nav-link">Contact</a>
+        {/* <a className="nav-link">Contact</a> */}
       </li>
     </ul>
   );
